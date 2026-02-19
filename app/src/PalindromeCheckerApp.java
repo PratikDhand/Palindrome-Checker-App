13a903ff4a4e9049666b8223bda58eddaf326bbe
rachita <rd1363@srmist.edu.in>
2026-02-19 10:37:55 +0530

    uc2

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
 /**
  MAIN CLASS UseCase1Palindrome App
@@ -11,14 +13,34 @@ public class PalindromeCheckerApp {
  Shows application version
 
  The goal is to establish a clear startup flow.
- @author Developer
- @version 1.0
+ @author PratikDhand
+ @version 2.0
  **/
-    public static void main(String[] args) {
-        System.out.println("welcome to Palindrome Checker system");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
+public static void main(String[] args) {
+    System.out.println("Welcome to Palindrome Checker App");
+    System.out.println("Version: 2.0");
+    System.out.println("System initialised successfully.");
+    System.out.println("--------------------------------------------");
+    System.out.println("Enter string to check if palindrome: ");
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    if(checkPalindrome(s))
+        System.out.println(s+" is a palindrome");
+    else
+        System.out.println(s+" is not a palindrome");
+}
+    public static boolean checkPalindrome(String s){
+        char[] arr = new char[s.length()];
+        arr = s.toCharArray();
+        for (int i = 0; i < s.length()/2; i++) {
+            if (arr[i] == arr[s.length()-i-1])
+                continue;
+            else
+                return false;
+        }
+        return true;
     }
 }
+
 
 
